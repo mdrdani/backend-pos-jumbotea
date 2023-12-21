5a506e4732f8e946821b3023942a0c9cfbbd9fe9
mdrdani <sudokill3004@example.com>
2023-12-21 15:20:30 +0700

    api auth login & product

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// POST Login
+Route::post('login', [AuthController::class, 'login']);
+
+// api resource product
+Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');
